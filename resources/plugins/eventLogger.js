02bddd92e614c44a5e07c3e3894c0bc81f14dd81
Matthias Mullie <git@mullie.eu>
2021-04-14 15:29:37 +0200

    Fix eventlogging-related dependencies
    The mediawiki.user (part of core so can require right away)
    module is required for the `mw.user.getPageviewToken()` call.
    The ext.eventLogging module is lazy-loaded because it
    requires another extension which may or may not be present.
    If it doesn't exist, logging will be a no-op instead of
    causing a failure.
    
    Bug: T277917
    Change-Id: I55071a9a39d490ae79e625cc1e3c497015b71c74

--- resources/plugins/eventLogger.js
+++ resources/plugins/eventLogger.js
@@ -11,12 +11,16 @@ module.exports = {
 
 		/* eslint-disable camelcase */
 		Vue.prototype.$log = function ( event ) {
-			event.$schema = schema;
-			event.web_pageview_id = token;
-			event.language_code = mw.language.getFallbackLanguageChain()[ 0 ];
-			event.ui_mw_skin = mw.config.get( 'skin' );
+			// if eventlogging is available, log this event (otherwise
+			// this becomes a no-op)
+			mw.loader.using( [ 'ext.eventLogging' ] ).then( function () {
+				event.$schema = schema;
+				event.web_pageview_id = token;
+				event.language_code = mw.language.getFallbackLanguageChain()[ 0 ];
+				event.ui_mw_skin = mw.config.get( 'skin' );
 
-			mw.eventLog.submit( stream, event );
+				mw.eventLog.submit( stream, event );
+			} );
 		};
 		/* eslint-enable camelcase */
 	}
