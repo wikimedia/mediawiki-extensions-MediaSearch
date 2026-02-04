5a9efe6e3923c243c74b94946792db8116faff0b
Amir Sarabadani <Ladsgroup@gmail.com>
2026-02-04 22:30:46 +0100

    Stop relying on ThumbRenderMap and use a standard size instead
    We can't really use the standard thumb steps config since the min
    function means it'll start showing everything at 20px instead. So let's
    just simplify everything.
    
    Bug: T415282
    Change-Id: If2612bc0348c939cba80e8b6539b640a0e591ce2
    (cherry picked from commit 432cd4496f5147d2a970169d17fe79b7bf051922)

--- tests/jest/store/actions.test.js
+++ tests/jest/store/actions.test.js
@@ -3,7 +3,6 @@ const initialState = require( '../fixtures/initialVuexState.js' ),
 	mockImageSearchApiResponse = require( '../fixtures/mockImageSearchApiResponse.json' ),
 	mockEmptyImageSearchApiWithSuggestionResponse = require( '../fixtures/mockEmptyImageSearchApiWithSuggestion.json' ),
 	mockImageDetailsApiResponse = require( '../fixtures/mockImageDetailsApiResponse.json' ),
-	thumbRenderMap = [ 320, 640, 800, 1024, 1280, 1920 ],
 	when = require( 'jest-when' ).when;
 
 require( '../mocks/history.js' );
@@ -48,7 +47,6 @@ describe( 'performNewSearch', () => {
 		// Mocks for mw.config values
 		when( global.mw.config.get ).calledWith( 'wgUserLanguage' ).mockReturnValue( 'en' );
 		when( global.mw.config.get ).calledWith( 'sdmsNamespaceGroups' ).mockReturnValue( namespaceGroups );
-		when( global.mw.config.get ).calledWith( 'sdmsThumbRenderMap' ).mockReturnValue( thumbRenderMap );
 		when( global.mw.config.get ).calledWith( 'sdmsExternalSearchUri' ).mockReturnValue( false );
 
 		// Fake an API response
@@ -123,7 +121,7 @@ describe( 'performNewSearch', () => {
 					inprop: 'url',
 					iiprop: 'url|size|mime',
 					iiurlheight: undefined,
-					iiurlwidth: 320,
+					iiurlwidth: 330,
 					wbetterms: 'label'
 				} )
 			);
@@ -147,7 +145,7 @@ describe( 'performNewSearch', () => {
 					inprop: 'url',
 					iiprop: 'url|size|mime',
 					iiurlheight: undefined,
-					iiurlwidth: 200,
+					iiurlwidth: 250,
 					wbetterms: 'label'
 				} )
 			);
@@ -328,7 +326,6 @@ describe( 'searchMore', () => {
 		// Mocks for mw.config values
 		when( global.mw.config.get ).calledWith( 'wgUserLanguage' ).mockReturnValue( 'en' );
 		when( global.mw.config.get ).calledWith( 'sdmsNamespaceGroups' ).mockReturnValue( namespaceGroups );
-		when( global.mw.config.get ).calledWith( 'sdmsThumbRenderMap' ).mockReturnValue( thumbRenderMap );
 		when( global.mw.config.get ).calledWith( 'sdmsExternalSearchUri' ).mockReturnValue( false );
 
 		// Fake an API response
