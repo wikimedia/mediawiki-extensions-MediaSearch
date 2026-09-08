328027bdac699edef832bc04ad321fd6463384ce
SBassett <sbassett@wikimedia.org>
2026-09-08 20:35:51 +0000

    Revert "Filter out non-http(s) license urls"
    This reverts commit 632a24fc423091ab1996a491f317f90ac97e22e7.
    
    Reason for revert: Caused production error
    
    Change-Id: I2d0f62cdee8aa70d99e29b4a48d9b61f3b7352e9

--- resources/components/QuickView.vue
+++ resources/components/QuickView.vue
@@ -482,17 +482,10 @@ module.exports = exports = {
 
 		licenseUrl: function () {
 			if ( this.metadata && this.metadata.LicenseUrl ) {
-				try {
-					const url = new URL( this.metadata.LicenseUrl.value );
-					// Filter out non-http(s) license urls
-					// https://phabricator.wikimedia.org/T435999
-					if ( [ 'http:', 'https:' ].includes( url.protocol ) ) {
-						return this.metadata.LicenseUrl.value;
-					}
-				} catch ( e ) {
-				}
+				return this.metadata.LicenseUrl.value;
+			} else {
+				return null;
 			}
-			return null;
 		},
 
 		/**
